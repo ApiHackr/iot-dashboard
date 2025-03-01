6d047a8fa6164ccbe2107342162273a85b4ee9e0
ApiHackr <elz.eth.hacker@gmail.com>
2025-03-01 16:25:32 +0100

    update app.js

--- app.js
+++ app.js
@@ -5,4 +5,15 @@ const DEVICES_PER_PAGE = 5;
 
 document.getElementById("device-form").addEventListener("submit", e => {
     e.preventDefault();
+    const device = {
+        id: document.getElementById("device-id").value,
+        type: document.getElementById("sensor-type").value,
+        location: document.getElementById("location").value,
+        apiEndpoint: document.getElementById("api-endpoint").value,
+        isActive: true
+    };
+    const devices = JSON.parse(localStorage.getItem("device")) || [];
+    devices.push(device);
+    localeStorage.setItem("device", JSON.stringify(devices));
+    renderDevices();
 })
